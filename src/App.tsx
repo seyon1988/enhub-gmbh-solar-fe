c1b7dcaac2b1655ce06611be78de333d229b9a59
rseyo <rseyo@ou.ac.lk>
2026-08-11 13:14:12 +0530

    Double electrical signal density (10 active signal paths)

--- src/App.tsx
+++ src/App.tsx
@@ -84,6 +84,11 @@ function App() {
         <div className="electric-line line-3"></div>
         <div className="electric-line line-4"></div>
         <div className="electric-line line-5"></div>
+        <div className="electric-line line-6"></div>
+        <div className="electric-line line-7"></div>
+        <div className="electric-line line-8"></div>
+        <div className="electric-line line-9"></div>
+        <div className="electric-line line-10"></div>
       </div>
 
       <header>
